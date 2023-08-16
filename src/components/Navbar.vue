7996abc5dd7685f3ba3206a65e71003eabfe567e
kidiim <21221753112@smkassalaambandung.sch.id>
2023-08-16 13:53:00 +0700

    nambah

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
       
   <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" class="flex items-center">
+    <a href="https://wa.wizard.id/2734a0" class="flex items-center">
         <img src="../assets/img/logo.png" class="h-8 mr-3" alt="Flowbite Logo">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dimasss.</span>
     </a>
@@ -34,6 +34,9 @@
         </li>
         <li>
           <a href="/produk" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Product</a>
+        </li>
+        <li>
+          <a href="/contact" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
         </li>
       </ul>
     </div>
--- src/views/Produk.vue
+++ src/views/Produk.vue
@@ -33,7 +33,9 @@
                 <span class="text-teal-400 font-bold text-xl mb-3">Compass Retrograde</span>
                 <img class="h-16 w-16 rounded-full shadow-2xl" src="../assets/img/compass.jpeg" alt="Img">
                 <p class="mt-3 text-gray-600 text-center">"Sepatu Compass bestseller banyak di cari oleh konsumen dan ketceee"</p>
+                <router-link to="/Singleproduk">
                 <button class="mt-4 mb-2 bg-teal-400 rounded-full px-12 py-1 text-gray-100 font-semibold hover:bg-teal-300 focus:outline-none">Detail</button>
+              </router-link>
             </div>
 
             <div class="carousel-focus flex items-center flex-col relative bg-white mx-5 my-10 px-4 py-3 rounded-lg shadow-lg" style="width: 270px;">
@@ -111,13 +113,13 @@
 <center>
     <h1
       class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-     PRODUK KAMI
+     PRODUK DENGAN PENJUALAN TERBAIK  
     </h1>
   </center>
  
     <div class="grid grid-cols-3 gap-8 ml-12">
     <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-  <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+  <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="/Singleproduk">
     <img class="peer absolute top-0 right-0 h-full w-full object-cover" src="../assets/img/compass.jpeg" alt="product image" />
     <svg class="pointer-events-none absolute inset-x-0 bottom-5 mx-auto text-3xl text-white  transition-opacity group-hover:animate-ping group-hover:opacity-30 peer-hover:opacity-0" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M2 10a4 4 0 0 1 4-4h20a4 4 0 0 1 4 4v10a4 4 0 0 1-2.328 3.635a2.996 2.996 0 0 0-.55-.756l-8-8A3 3 0 0 0 14 17v7H6a4 4 0 0 1-4-4V10Zm14 19a1 1 0 0 0 1.8.6l2.7-3.6H25a1 1 0 0 0 .707-1.707l-8-8A1 1 0 0 0 16 17v12Z" /></svg>
   </a>
@@ -134,7 +136,7 @@
         <span class="text-sm text-slate-900 line-through">Rp.2.000.000</span>
       </p>
     </div>
-    <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+    <a href="/cart" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
       <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
       </svg>
@@ -260,17 +262,7 @@
 </div>
 </div>
 
-<!-- Footer -->
-<footer class="relative mt-20 bg-gray-900 px-4 pt-20">
-  <div class="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-xl border-4 border-sky-500 bg-white p-2"><img class="h-full object-contain" src="../assets/img/logo1.png" alt="" /></div>
-  <nav aria-label="Footer Navigation" class="mx-auto mb-10 flex max-w-lg flex-col gap-10 text-center sm:flex-row sm:text-left">
-    <a href="#" class="font-medium text-white">Demo</a>
-    <a href="#" class="font-medium text-white">Support</a>
-    <a href="#" class="font-medium text-white">Privacy Policy</a>
-    <a href="#" class="font-medium text-white">Terms & Conditions</a>
-  </nav>
-  <p class="py-10 text-center text-gray-300">© Copyright 2023 | Diimzyy</p>
-</footer>
+
 
 
 </template>
