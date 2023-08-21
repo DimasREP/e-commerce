dd13f8c9754b13d8e04913a02faad82776843b4e
kidiim <21221753112@smkassalaambandung.sch.id>
2023-08-21 14:58:59 +0700

    push

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ import Produk from "../views/Produk.vue";
 import Contact from "../views/Contact.vue";
 import Cart from "../views/Cart.vue";
 import Checkout from "../views/Checkout.vue";
-import SingleProduk from "../views/SingleProduct.vue";
 import Brands from "../views/Brands.vue";
 import Category from "../views/Category.vue";
 
@@ -20,11 +19,6 @@ const routes = [
         path: "/produk",
         name: "Produk",
         component: Produk
-    },
-    {
-        path: "/singleproduk",
-        name: "Singleproduk",
-        component: SingleProduk
     },
     {
         path: "/cart",
