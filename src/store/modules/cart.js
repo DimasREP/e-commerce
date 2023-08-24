9bea4af77b683e6e129a3719ccfc1e2e87adf2c2
kidiim <21221753112@smkassalaambandung.sch.id>
2023-08-24 08:39:26 +0700

    nambah cart

---
+++ src/store/modules/cart.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const cart = {
+    namespaced: true,
+    state: {
+       cart : []
+    },
+    getters: {
+       getCart: (state) => state.cart
+    },
+    actions: {
+        async fetchCart({ commit }) {
+            try{
+                // const token = localStorage.getItem('token');
+                const dataCart = await axios.post(
+                    "https://ecommerce.olipiskandar.com/api/v1/carts",{
+                        "temp_user_id" : null
+                    }, {
+                    headers : {
+                        'Authorization' : `Bearer ${localStorage.getItem('token')}`
+                    }
+                    }
+
+                );
+                console.log(dataCart.data['cart_items'])
+                commit("SET_CART", dataCart.data['cart_items']);
+            } catch (error) {
+                alert(error);
+                console.log(error);
+            }
+        },
+    },
+    mutations: {
+        SET_CART(state, cart) {
+            state.dataBrands = cart;
+        }
+    },
+};
+export default cart;
