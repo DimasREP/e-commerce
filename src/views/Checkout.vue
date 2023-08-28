76ab5e571424f24157c69058ab0667297efa310a
DimasREP <21221753112@smkassalaambandung.sch.id>
2023-08-28 14:30:29 +0700

    tambah order

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -51,7 +51,7 @@
       <p class="mt-8 text-lg font-medium">Metode Pembayaran</p>
       <form class="mt-5 grid gap-6">
         <div class="relative">
-          <input class="peer hidden" id="radio_1" type="radio" name="radio"  />
+          <input class="peer hidden" id="radio_1" type="radio" value="cash_on_delivery" v-model="paymentType" name="radio"  />
           <span
             class="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
           <label
@@ -68,7 +68,7 @@
         </div>
 
         <div class="relative">
-          <input class="peer hidden" id="radio_2" type="radio" name="radio"  />
+          <input class="peer hidden" id="radio_2" type="radio" name="radio"   />
           <span
             class="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
           <label
@@ -84,12 +84,12 @@
         </div>
       </form>
 
-      <p class="mt-8 text-lg font-medium">Delivery Option</p>
+      <p class="mt-8 text-lg font-medium">Metode Pengiriman</p>
     <form class="mt-3 grid gap-6">
       <div class="flex flex-row m3">
       <div class="basis-1/2 m-1">
       <div class="relative">
-        <input class="peer hidden" v-model="deliveryType" value="standard" id="radio_3" type="radio" name="radio"  />
+        <input class="peer hidden" v-model="deliveryType" value="express" id="radio_3" type="radio" name="radio"  />
         <span class="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
         <label class="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" for="radio_3">
           <img class="w-14 object-contain"
@@ -100,19 +100,19 @@
             <p class="text-slate-500 text-sm leading-6">Delivery: 1-2 Days</p>
           </div>
         </label>
-      </div>
+      </div>    
       </div>
 
       <div class="basis-1/2 m-1">
       <div class="relative">
-        <input class="peer hidden" v-model="deliveryType" value="express" id="radio_4" type="radio" name="radio"  />
+        <input class="peer hidden" v-model="deliveryType" value="standard" id="radio_4" type="radio" name="radio"  />
         <span class="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
         <label class="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" for="radio_4">
           <img class="w-14 object-contain"
               src="https://componentland.com/images/naorrAeygcJzX0SyNI4Y0.png"
               alt="" />
           <div class="ml-5">
-            <span class="mt-2 font-semibold">Reguller</span>
+            <span class="mt-2 font-semibold">Standard</span>
             <p class="text-slate-500 text-sm leading-6">Delivery: 2-7 Days</p>
           </div>
         </label>
